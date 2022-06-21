e8791ef949eb34b15e6a10fff2a759baa7451769
Akim <egorov.akim@mail.ru>
2022-06-21 14:09:06 +0300

    default version

--- app/models.py
+++ app/models.py
@@ -3,9 +3,12 @@ from random import random, randint
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
-from coolname import generate_slug
+#from coolname import generate_slug
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
+from django.utils.timezone import now
 
 TITLE_LENGTH = 1024
 CONTENT_LENGTH = 65536
@@ -16,90 +19,68 @@ NUMBER_OF_QUESTIONS = 200
 NUMBER_OF_ANSWERS = 2000
 NUMBER_OF_TAGS = 20
 
-LOREM_IPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque tempor id risus vel facilisis. Nunc " \
-              "commodo non orci a mattis. Fusce nulla erat, mollis non ipsum ac, finibus accumsan ligula. Nam et " \
-              "nulla eget neque consequat imperdiet. Ut pharetra odio aliquam lacinia lacinia. Curabitur non dui sed " \
-              "est finibus tempor nec vitae dui. Donec fermentum leo arcu, nec finibus mi pretium nec. Proin finibus " \
-              "semper purus vel convallis. Quisque eget fermentum dui. Morbi sollicitudin sit amet odio eget " \
-              "dignissim. Curabitur nec nisi hendrerit neque rhoncus egestas at quis tellus. Pellentesque quam sem, " \
-              "elementum eu sapien a, iaculis cursus lectus. Etiam ut nulla vel est ultricies fringilla. Maecenas " \
-              "pretium ultricies nibh, efficitur cursus ante volutpat sit amet. "
-
-
-# Create your models here.
-class TagManager(models.Manager):
-    pass
-
 
 class Tag(models.Model):
-    tag = models.CharField(max_length=TAG_LENGTH, blank=True)
-    objects = TagManager()
-
-
-class ProfileManager(models.Manager):
-    pass
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='images', null=True)
-    bio = models.TextField(null=True)
-    questions = models.ForeignKey('Question', on_delete=models.CASCADE)
-
-    objects = ProfileManager()
-
-class AnswerManager(models.Manager):
-    pass
-
-
-class Answer(models.Model):
-    question = models.ForeignKey('Question', on_delete=models.CASCADE)
-    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    content = models.TextField(blank=True)
-    user_rating = models.IntegerField(null=True)
-
-    objects = AnswerManager()
-
-
-class QuestionManager(models.Manager):
-    def get_tags(self, question_id: int):
-        return Tag.objects.filter(question_id=question_id).all().values()
-
-    def get_hot(self):
-        return self.filter(hot=True)
-
-    def get_popular(self):
-        return self.filter(number_of_answers=10)
-
-    def get_question_by_id(self, question_id):
-        return self.filter(id=question_id)
-
-    def get_questions_by_user_id(self, user_id):
-        return Profile.objects.filter(id=user_id)
-
-    def get_questionos_by_tag(self, tag: str):
-        return self.get_tag
-
-    def get_questions_tags(self, question_id: int):
-        return Tag.objects.filter(question_id=question_id)
-
-    def get_question_answers(self, question_id: int):
-        return Answer.objects.filter(question_id=question_id)
-
-
-class Question(models.Model):
-    title = models.CharField(max_length=TITLE_LENGTH, blank=True)
-    content = models.CharField(max_length=CONTENT_LENGTH, blank=True)
-    tags = models.ForeignKey(Tag, on_delete=models.DO_NOTHING)
-    hot = models.BooleanField()
-    published_date = models.DateTimeField()
-    number_of_answers = models.IntegerField(null=True)
-
-    # user_rating = models.IntegerField(len(list(Answer.question.id=id))
-
-    objects = QuestionManager()
+    title = models.CharField(max_length=64, verbose_name="Tag")
 
     def __str__(self):
         return self.title
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='images', null=True, blank=True,)
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Answer(models.Model):
+    question = models.ForeignKey('Question', related_name='answers', on_delete=models.CASCADE, default=1)
+    author = models.ForeignKey(Profile, related_name='answers', on_delete=models.CASCADE, default=1)
+    content = models.TextField(blank=True)
+    is_correct = models.BooleanField(default=False)
+    user_rating = models.IntegerField(null=True)
+
+
+class Like(models.Model):
+    user = models.ForeignKey(Profile, related_name='likes', on_delete=models.CASCADE)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+
+    object_id = models.PositiveIntegerField()
+    like_object = GenericForeignKey('content_type', 'object_id')
+
+
+class QuestionManager(models.Manager):
+    def get_popular(self):
+        return self.filter(likes__gt=10)
+
+    def get_recent(self):
+        return self.filter(published_date__gt=now())
+
+    def get_question_by_id(self, question_id):
+        return self.filter(id=question_id)
+
+    def get_questions_by_user_id(self, user_id):
+        return self.filter(author__user=user_id)
+
+    def get_questions_by_tag_title(self, title):
+        return self.filter(tags__title=title)
+
+    def get_question_answers(self, question_id: int):
+        return self.filter(answers__author__questions=question_id)
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=TITLE_LENGTH, blank=True)
+    content = models.CharField(max_length=CONTENT_LENGTH, blank=True)
+    author = models.ForeignKey(Profile, related_name='questions', on_delete=models.CASCADE, default=1)
+    published_date = models.DateTimeField(blank=True, auto_now=True)
+    number_of_answers = models.IntegerField(null=True)
+    tags = models.ManyToManyField(Tag, related_name='questions')
+
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
